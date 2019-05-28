d3e2ef706384b8655dee7dbf5a5fb5cf5f99f1cf
marvin banton <marvin.banton@boom.camp>
2019-05-28 14:18:50 +0800

    Done Problem 2

--- practice.js
+++ practice.js
@@ -50,7 +50,9 @@ first(names, function(firstName){
 */
 
 //Code Here
-
+function last (arr, cb) {
+  cb(arr[arr.length -1]);
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
