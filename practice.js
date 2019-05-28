2e1f9f963b4dddec829789a1d27fac5158f377cc
Noel Layante <noel.layante@boom.camp>
2019-05-28 13:57:51 +0800

    Done Problem 3

--- practice.js
+++ practice.js
@@ -72,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, callback) {
+  callback(num1*num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
