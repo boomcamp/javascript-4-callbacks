8288354e9b9353b63e1c847ab159d339a204dbae
jomar-09 <jomar2999@gmail.com>
2019-05-28 14:55:58 +0800

    solved prob 5

--- practice.js
+++ practice.js
@@ -120,7 +120,20 @@ contains(names, 'Oscar', function(result){
 */
 
 //Code Here
-
+var uniq = function(arr, cb){
+  var c = 1;
+  for (let i=0; i<arr.length; i++){
+    
+    for (let j=c; j<arr.length; j++){
+        
+        if(arr[i] === arr[j]){
+           arr.splice(j, 1);
+        }
+    } 
+    c++;
+  }
+  cb(arr);
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
